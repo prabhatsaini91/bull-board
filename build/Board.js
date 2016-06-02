94cfe5226efa4b12df1bbb5d540948cf2298ccc5
prabhatsaini91 <prabhat.saini91@gmail.com>
2016-06-02 14:51:07 +0530

    updated to use es6

--- build/Board.js
+++ build/Board.js
@@ -1,69 +1,53 @@
-var Board = React.createClass({displayName: "Board",
-    propTypes: {
-        count: function(props, propName) {
-            if (typeof props[propName] !== "number"){
-                return new Error('The count property must be a number');
-            }
-            if (props[propName] > 100) {
-                return new Error("Creating " + props[propName] + " notes is ridiculous");
-            }
-        }
-    },
-    getInitialState: function() {
-        return {
+
+class Board extends React.Component {
+    constructor() {
+        super();
+        this.update = this.update.bind(this);
+        this.remove = this.remove.bind(this);
+        this.eachNote = this.eachNote.bind(this);
+        this.add = this.add.bind(this);
+        this.state = {
             notes: []
         };
-    },
-    nextId: function() {
-        this.uniqueId = this.uniqueId || 0;
+    }
+
+    nextId() {
+        this.uniqueId = this.uniqueId||0;
         return this.uniqueId++;
-    },
-    componentWillMount: function() {
-        var self = this;
-        if(this.props.count) {
-            $.getJSON("http://baconipsum.com/api/?type=all-meat&sentences=" +
-                this.props.count + "&start-with-lorem=1&callback=?", function(results){
-                    results[0].split('. ').forEach(function(sentence){
-                        self.add(sentence.substring(0,40));
-                    });
-                });
-        }
-    },
-    add: function(text) {
+    }
+
+    update(newText,i) {
         var arr = this.state.notes;
-        arr.push({
-            id: this.nextId(),
-            note: text
-        });
-        this.setState({notes: arr});
-    },
-    update: function(newText, i) {
-        var arr = this.state.notes;
+        console.log(newText);
         arr[i].note = newText;
         this.setState({notes:arr});
-    },
-    remove: function(i) {
-        var arr = this.state.notes;
-        arr.splice(i, 1);
-        this.setState({notes: arr});
-    },
-    eachNote: function(note, i) {
-        return (
-                React.createElement(Note, {key: note.id, 
-                    index: i, 
-                    onChange: this.update, 
-                    onRemove: this.remove
-                }, note.note)
-            );
-    },
-    render: function() {
-        return (React.createElement("div", {className: "board"}, 
-                    this.state.notes.map(this.eachNote), 
-                    React.createElement("button", {className: "btn btn-sm btn-success glyphicon glyphicon-plus", 
-                            onClick: this.add.bind(null, "New Note")})
-            )
+    }
 
+    remove(i) {
+        var arr = this.state.notes;
+        arr.splice(i,1);
+        this.setState({notes:arr});
+
+    }
+
+    add(text) {
+        var arr = this.state.notes;
+        arr.push({id: this.nextId(), note: 'New Note'});
+        this.setState({notes:arr});
+    }
+
+    eachNote(note,i) {
+        return (
+            <Note key={note.id} index={i} onChange={this.update} onRemove={this.remove}>{note.note}</Note>
+            );
+    }
+
+    render() {
+        return (
+            <div className = "board">
+                {this.state.notes.map(this.eachNote)}
+                <button className = "btn btn-sm btn-success glyphicon glyphicon-plus" onClick={this.add}/>
+            </div>
         );
     }
-});
-
+}
