d92a576e66f021d9edf367ca4a06bb98df1b2cae
prabhatsaini91 <prabhat.saini91@gmail.com>
2016-06-02 14:50:03 +0530

    updated to use es6

--- build/Note.js
+++ build/Note.js
@@ -1,54 +1,72 @@
-var Note = React.createClass({displayName: "Note",
-    getInitialState: function() {
-        return {editing: false}
-    },
-    componentWillMount: function() {
-        this.style = {
-            right: this.randomBetween(0, window.innerWidth - 150) + 'px',
-            top: this.randomBetween(0, window.innerHeight - 150) + 'px',
-            transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+class Note extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            editing: false
         };
-    },
-    componentDidMount: function(){
-        $(this.getDOMNode()).draggable();
-    },
-    randomBetween: function(min, max) {
+
+        this.edit = this.edit.bind(this);
+        this.save = this.save.bind(this);
+        this.remove = this.remove.bind(this);
+        this.renderDisplay = this.renderDisplay.bind(this);
+        this.renderForm = this.renderForm.bind(this);
+        
+
+        super();
+    }
+
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth-150) + 'px', 
+            top: this.randomBetween(0, window.innerHeight-150) + 'px',
+            // transform: 'rotate(' + this.randomBetween(-15,15) + 'deg)'
+        };
+    }
+
+    componentDidMount() {
+    	$(this.refs.noteRef).draggable({cursor: "move", containment: 'parent'});
+    	console.log("Did Mount");
+    }
+
+    randomBetween(min,max) {
         return (min + Math.ceil(Math.random() * max));
-    },
-    edit: function() {
-        this.setState({editing: true});
-    },
-    save: function() {
-        this.props.onChange(this.refs.newText.getDOMNode().value, this.props.index);
-        this.setState({editing: false});
-    },
-    remove: function() {
+    }
+
+    edit() {
+        this.setState({editing:true});
+    }
+
+    save() {
+        this.props.onChange(this.refs.newText.value, this.props.index);
+        this.setState({editing:false});
+    }
+
+    remove() {
         this.props.onRemove(this.props.index);
-    },
-    renderDisplay: function() {
+    }
+    
+    renderDisplay() {
         return (
-            React.createElement("div", {className: "note", 
-                style: this.style}, 
-                React.createElement("p", null, this.props.children), 
-                React.createElement("span", null, 
-                    React.createElement("button", {onClick: this.edit, 
-                            className: "btn btn-primary glyphicon glyphicon-pencil"}), 
-                    React.createElement("button", {onClick: this.remove, 
-                            className: "btn btn-danger glyphicon glyphicon-trash"})
-                )
-            )
-            );
-    },
-    renderForm: function() {
+            <div ref="noteRef" className = "note" style={this.style}>
+                <p>{this.props.children}</p>
+                <span> 
+                    <button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
+                    <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
+                </span>
+            </div>
+        );
+    }
+
+    renderForm() {
         return (
-            React.createElement("div", {className: "note", style: this.style}, 
-            React.createElement("textarea", {ref: "newText", defaultValue: this.props.children, 
-            className: "form-control"}), 
-            React.createElement("button", {onClick: this.save, className: "btn btn-success btn-sm glyphicon glyphicon-floppy-disk"})
-            )
-            )
-    },
-    render: function() {
+            <div className = "note" style={this.style}>
+                <textarea ref="newText" defaultValue={this.props.children} className="form-control"></textarea>
+                <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk"/>
+            </div>
+        );
+    }
+    
+    render() {
         if (this.state.editing) {
             return this.renderForm();
         }
@@ -56,17 +74,4 @@ var Note = React.createClass({displayName: "Note",
             return this.renderDisplay();
         }
     }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
